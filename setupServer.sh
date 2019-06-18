81e9501303503f729f4db1e2045f1bef36010785
Matt25969 <matt.jo.hunt@gmail.com>
2019-06-18 11:48:34 +0100

    Removed Node Setup and changed file location

--- setupServer.sh
+++ setupServer.sh
@@ -2,11 +2,9 @@
 sudo apt-get update
 sudo apt-get install unzip
 sudo apt install -y openjdk-8-jdk
-sudo apt-get install node.js -y
-sudo apt-get install npm -y
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
 rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
-cp WildflySetup/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
+cp WildflySetupBasic/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
 sudo sh wildfly-10.1.0.Final/bin/standalone.sh &
 
