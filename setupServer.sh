c9b78c72bf838457bec2165483f739867769d2b4
Matt25969 <matt.jo.hunt@gmail.com>
2019-06-18 12:08:18 +0100

    Added Maven installation

--- setupServer.sh
+++ setupServer.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install unzip
+sudo apt-get update
+sudo apt-get install maven -y
 sudo apt install -y openjdk-8-jdk
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
