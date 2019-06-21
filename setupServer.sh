22dd6161054c521013aa67f8676f31e62b9188c6
Matt25969 <matt.jo.hunt@gmail.com>
2019-06-21 08:09:34 +0100

    Demo

--- setupServer.sh
+++ setupServer.sh
@@ -8,6 +8,6 @@ cd /var/lib
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
 rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
-cp WildflySetupBasic/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
+cp ~/WildflySetupBasic/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
 sudo sh wildfly-10.1.0.Final/bin/standalone.sh &
 
