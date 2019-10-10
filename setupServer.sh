3e2a31911a11e07964b7d8b3345fc19880f02cf9
Matt25969 <matt.jo.hunt@gmail.com>
2019-10-10 12:05:10 +0100

    Update setupServer.sh

--- setupServer.sh
+++ setupServer.sh
@@ -4,7 +4,7 @@ sudo rm -R wildfly-10.1.0.Final
 sudo rm -R wildfly-10.1.0.Final.zip
 wget "https://download.jboss.org/wildfly/10.1.0.Final/wildfly-10.1.0.Final.zip"
 unzip wildfly-10.1.0.Final.zip
-rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
-cp /home/ubuntu/server/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
+sudo rm wildfly-10.1.0.Final/standalone/configuration/standalone.xml
+sudo cp /home/ubuntu/server/standalone.xml wildfly-10.1.0.Final/standalone/configuration/
 sudo sh wildfly-10.1.0.Final/bin/standalone.sh &
 
